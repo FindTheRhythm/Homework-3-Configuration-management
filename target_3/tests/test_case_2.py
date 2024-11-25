3851c521592a947d4a6029e335a1ea7d0ae625ee
Данила FindTheRhythm <140545017+FindTheRhythm@users.noreply.github.com>
2024-11-26 01:58:56 +0300

    Final test_case_2.py

--- target_3/tests/test_case_2.py
+++ target_3/tests/test_case_2.py
@@ -1,1 +1,25 @@
+import unittest
+import json
+from config_parser import ConfigParser
+
+class TestCalculation(unittest.TestCase):
+    def test_calculation_parsing_with_error(self):
+        test_input = """
+        (define MAX_SIZE 10);
+        (define OFFSET 5);
+        @(OFFSET 3 - abs)
+        """
+
+        expected_data = [
+            {"define": {"name": "MAX_SIZE", "value": "10"}},
+            {"define": {"name": "OFFSET", "value": "5"}},
+        ]
+
+        expected_errors = [
+            "ERROR:config_parser:Ошибка вычисления: 5 3 - abs (Неизвестный токен: -)"
+        ]
+
+
+if __name__ == "__main__":
+    unittest.main()
 
